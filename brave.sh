18e91a2781ae1ca25b9e04e95f60370efe852790
murunwas <cengyweb@gmail.com>
2021-01-09 00:05:33 +0200

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -5,8 +5,19 @@ curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 sudo apt update
-
+echo "Installing brave-browser, please wait..."
 sudo apt install brave-browser
 
 sudo apt install gdebi
 sudo apt install path_to_deb_file
+
+#Install vscode...
+echo "Installing vscode, please wait..."
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code 
