cb56ba934fd2a288922169dd1ea16dca2dfa30e8
murunwas <cengyweb@gmail.com>
2021-11-04 11:00:34 +0200

    Create installNode.sh

---
+++ installNode.sh
@@ -0,0 +1,10 @@
+echo "Installing curl"
+apt install curl
+echo "Installing NVM"
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.profile
+echo "Installing nodejs"
+nvm install node
+echo "Installing webtorent..."
+npm install webtorrent-cli -g
+echo "complete setup...."
